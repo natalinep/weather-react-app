82a9dbb3c90cc9b31447176951e26db59a3f42fa
natalinep <nep.natali@gmail.com>
2022-08-18 12:35:25 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="container"> </div>
+    </div>
+  );
 }
